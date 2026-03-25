afbe9212280c9e52359f64857e58e63c93e0f41c
Otto-mars <8mehmetalicalis8@gmail.com>
2026-03-25 12:01:07 +0300

    Update main.cpp

--- LAB-3/main.cpp
+++ LAB-3/main.cpp
@@ -11,7 +11,8 @@ Song *next;
 Song *prev;
 };
 
-
+Song *head    = nullptr;
+Song *current = nullptr;
 
 void add(string title, string artist, int r0, int r1, int r2) {
     Song *yeni_sarki = new Song();
@@ -37,14 +38,81 @@ void add(string title, string artist, int r0, int r1, int r2) {
 
 
 
+void next() {
+    if (current != nullptr && current->next != nullptr)
+        current = current->next;
+    else
+        cout << "Zaten son sarkidasin." << endl;
+}
+
+void prev() {
+    if (current != nullptr && current->prev != nullptr)
+        current = current->prev;
+    else
+        cout << "Zaten ilk sarkidasin." << endl;
+}
+
+void removeCurrent() {
+    if (current == nullptr) return;
+
+    Song *silinecek = current;
+
+    if (silinecek->prev != nullptr)
+        silinecek->prev->next = silinecek->next;
+    else
+        head = silinecek->next;
+
+    if (silinecek->next != nullptr)
+        silinecek->next->prev = silinecek->prev;
+
+    if (silinecek->next != nullptr)
+        current = silinecek->next;
+    else
+        current = silinecek->prev;
+
+    delete silinecek;
+}
+
+
+void display() {
+    Song *ptr = head;
+    while (ptr != nullptr) {
+        if (ptr == current) cout << "-> ";
+        else                cout << "   ";
+
+        cout << ptr->title << " / " << ptr->artist << "  Ratings: [";
+        for (int i = 0; i < 3; i++) {
+            cout << ptr->ratings[i];
+            if (i < 2) cout << ", ";
+        }
+        cout << "]" << endl;
+
+        ptr = ptr->next;
+    }
+}
+
+
+
 int main (){
 
+    add("Itri Segah", "Buhurizade Itri", 9, 8, 10);
+    add("Nihavend Longa", "Tatyos Efendi", 7, 9, 8);
+    add("Huseyni Sazsemaisi", "Dede Efendi", 10, 9, 9);
 
+    cout << "--- Initial Playlist ---" << endl;
+    display();
 
+    cout << "\n--- Move to Next ---" << endl;
+    next();
+    display();
 
+    cout << "\n--- Remove Current ---" << endl;
+    removeCurrent();
+    display();
 
-
-
+    cout << "\n--- Move to Prev ---" << endl;
+    prev();
+    display();
 
 
 return 0;
